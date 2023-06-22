19c3dc805e7f8545d2fd5e951bdb50cdd8d27125
kairi003 <kairi.satellite@gmail.com>
2023-06-22 19:51:29 +0000

    バックグラウンド処理のAPIへの実装

--- app/bgproc/gcva.py
+++ app/bgproc/gcva.py
@@ -5,13 +5,15 @@ from google.oauth2 import service_account
 import io
 import os
 
+
 class GcvaAnalyze(BackgroundAnalyzeProcessBase):
     def __init__(self, json_path: str) -> None:
         self.json_path = json_path
 
     def run(self, image_path: str) -> list[list[int]]:
         # 身元証明書のjson読み込み(GCVA)
-        credentials = service_account.Credentials.from_service_account_file(self.json_path)
+        credentials = service_account.Credentials.from_service_account_file(
+            self.json_path)
         client = vision.ImageAnnotatorClient(credentials=credentials)
 
         # Loads the image into memory(GCVA)
@@ -21,14 +23,14 @@ class GcvaAnalyze(BackgroundAnalyzeProcessBase):
         image = vision.Image(content=content)
 
         # Performs label detection on the image file(GCVA)
-        response =  client.document_text_detection(
-                image=image,
-                image_context={'language_hints': ['ja']}
-            )
-        
+        response = client.document_text_detection(
+            image=image,
+            image_context={'language_hints': ['ja']}
+        )
+
         # レスポンスからテキストデータを抽出(GCVA)
-        #output_text = ''
-        #for page in response.full_text_annotation.pages:
+        # output_text = ''
+        # for page in response.full_text_annotation.pages:
         #    for block in page.blocks:
         #        for paragraph in block.paragraphs:
         #            for word in paragraph.words:
@@ -36,19 +38,19 @@ class GcvaAnalyze(BackgroundAnalyzeProcessBase):
         #                    symbol.text for symbol in word.symbols
         #                ])
         #            output_text += '\n'
-        #print(output_text)
+        # print(output_text)
 
         # レスポンスからテキストの位置座標を抽出(GCVA)
-        output=[]
-        count=0
+        output = []
+        count = 0
         for texts in response.text_annotations:
-            vertices = [(vertex.x, vertex.y) for vertex in texts.bounding_poly.vertices]
-            x1=min(vertices[0][0],vertices[3][0])
-            y1=min(vertices[0][1],vertices[1][1])
-            x2=max(vertices[1][0],vertices[2][0])
-            y2=max(vertices[2][1],vertices[3][1])
-            if count !=0:
-                output.append([14,x1,y1,x2,y2])
+            vertices = [(vertex.x, vertex.y)
+                        for vertex in texts.bounding_poly.vertices]
+            x1 = min(vertices[0][0], vertices[3][0])
+            y1 = min(vertices[0][1], vertices[1][1])
+            x2 = max(vertices[1][0], vertices[2][0])
+            y2 = max(vertices[2][1], vertices[3][1])
+            if count != 0:
+                output.append([14, x1, y1, x2, y2])
             count += 1
-        return(output)
-
+        return (output)
---
+++ app/bgproc/proc.py
@@ -0,0 +1,72 @@
+# GCVA_YOLOのmain処理みたいな単体テスト等はココで実行する
+from . import YoloAnalyze
+from . import GcvaAnalyze
+from models import AnalyzeResult, AnalyzeResultList
+
+from . import post_processing
+
+
+def processing(target_path: str) -> list[AnalyzeResult]:
+    # 身元証明書のjson path
+    JSON_PATH = "/app/bgproc/content/google_cloud_account_service.json"
+    # The name of the image file to annotate
+    ORIGINAL_MODEL = "yolov8n.pt"
+    CUSTOM_MODEL = "/app/bgproc/content/train_model/using_model.pt"
+
+    original_yolo = YoloAnalyze(ORIGINAL_MODEL)
+    gcva = GcvaAnalyze(JSON_PATH)
+    custom_yolo = YoloAnalyze(CUSTOM_MODEL)
+
+    output = [
+        *original_yolo.run(target_path, 0),
+        *gcva.run(target_path),
+        *custom_yolo.run(target_path, 15),
+    ]
+    print(output)
+
+    # ===========post processing===========
+    # parameters
+    list_danger = [
+        {0, 1, 2, 3, 7, 9, 10, 11, 12, 13, 16},
+        {4, 5, 6, 8, 14, 15, 16, 17, 18},
+    ]
+    param1 = 3
+    param2 = 3
+    param = 0.8
+    cls_utilitypole = 18
+    cls_bluesign = 17
+    cls_letter = 14
+    list_labels = [
+        "person",
+        "bicycle",
+        "car",
+        "motorcycle",
+        "airplane",
+        "bus",
+        "train",
+        "trunk",
+        "boat",
+        "traffic light",
+        "fire hydrant",
+        "stop sign",
+        "parking meter",
+        "clock",
+        "letter",
+        "manhole",
+        "sun glass",
+        "blue sign",
+        "utility pole",
+    ]
+
+    output = post_processing.except_person(output)
+    output = post_processing.evaluate_danger(output, list_danger)
+    output = post_processing.detect_duplicate(output, param1, param2)
+    output = post_processing.find_letter_in_BandU(
+        output, param, cls_utilitypole, cls_bluesign, cls_letter
+    )
+    output = post_processing.from_labes_to_objects(output, list_labels)
+    output = post_processing.coord2size(output)
+    # 辞書型にするなら
+    output = post_processing.list2dictionaly(output)
+    output = AnalyzeResultList.parse_obj(output)
+    return output

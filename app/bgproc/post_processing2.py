b48fc423e01ff311296b7fedf0c772fe3551909a
kairi003 <kairi.satellite@gmail.com>
2023-06-23 12:33:34 +0000

    detect_duplicate -> merge_utility_poles

--- app/bgproc/post_processing2.py
+++ app/bgproc/post_processing2.py
@@ -1,6 +1,7 @@
 import itertools
 from models import AnalyzeResult
 from typing import Iterable
+from collections import deque
 
 
 def except_low_rate(result_list: list[AnalyzeResult], threshold: float):
@@ -13,6 +14,88 @@ def evaluate_danger(result_list: list[AnalyzeResult], danger_map: dict[str, int]
     for res in result_list:
         res.rate = danger_map.get(res.name, 0)
     return result_list
+
+
+def merge_utility_poles(result_list: list[AnalyzeResult], coef: float):
+    """
+    x軸方向に近い電柱をマージする
+
+    x軸方向に近いとは、x1, x2の差が幅の平均のcoef倍より小さいことを指す
+    """
+
+    def is_x_axis_close(
+        pole_i: AnalyzeResult, pole_j: AnalyzeResult, coef: float
+    ) -> bool:
+        """
+        2つの電柱がx軸方向に近いかどうかを判定する
+
+        x軸方向に近いとは、x1, x2の差がwidth_aveのthreshold倍より小さいことを指す
+        """
+
+        x1_i = pole_i.bounding_box.x
+        w_i = pole_i.bounding_box.w
+        x2_i = x1_i + w_i
+
+        x1_j = pole_j.bounding_box.x
+        w_j = pole_j.bounding_box.w
+        x2_j = x1_j + w_j
+
+        threshold = (w_i + w_j) / 2 * coef
+
+        return abs(x1_i - x1_j) < threshold and abs(x2_i - x2_j) < threshold
+
+    def merge(pole_i: AnalyzeResult, pole_j: AnalyzeResult) -> AnalyzeResult:
+        x1 = min(pole_i.bounding_box.x, pole_j.bounding_box.x)
+        x2 = max(
+            pole_i.bounding_box.x + pole_i.bounding_box.w,
+            pole_j.bounding_box.x + pole_j.bounding_box.w,
+        )
+        y1 = min(pole_i.bounding_box.y, pole_j.bounding_box.y)
+        y2 = max(
+            pole_i.bounding_box.y + pole_i.bounding_box.h,
+            pole_j.bounding_box.y + pole_j.bounding_box.h,
+        )
+        pole_i.bounding_box.x = x1
+        pole_i.bounding_box.y = y1
+        pole_i.bounding_box.w = x2 - x1
+        pole_i.bounding_box.h = y2 - y1
+        return pole_i
+
+    pole_results = [res for res in result_list if res.name == "utility pole"]
+    other_results = [res for res in result_list if res.name != "utility pole"]
+
+    # confidenceが高い順のキュー
+    pole_results = deque(
+        sorted(pole_results, key=lambda res: res.confidence, reverse=True)
+    )
+    merged_pole_results = []
+
+    while pole_results:
+        # confidenceが最大のものを取り出す
+        pole_i = pole_results.popleft()
+
+        # 次のループで使うキュー
+        new_pole_results = deque()
+        update_flag = False
+
+        for pole_j in pole_results:
+            if is_x_axis_close(pole_i, pole_j, coef):
+                # x軸方向に近い場合はマージ
+                pole_i = merge(pole_i, pole_j)
+                update_flag = True
+            else:
+                new_pole_results.append(pole_j)
+
+        if update_flag:
+            # 更新があった場合は先頭に追加
+            new_pole_results.appendleft(pole_i)
+        else:
+            # 更新がなかった場合はマージ済みリストに追加
+            merged_pole_results.append(pole_i)
+
+        pole_results = new_pole_results
+
+    return other_results + merged_pole_results
 
 
 # find_letter_jn_BandU
@@ -71,8 +154,18 @@ def find_letter_in_BandU(
         target_names (list[str]): 重なりを判定する物体の名前
     """
 
-    def object_contains_letter(object: AnalyzeResult, letter: AnalyzeResult, threthold: float) -> bool:
+    def object_contains_letter(
+        object: AnalyzeResult, letter: AnalyzeResult, threthold: float
+    ) -> bool:
         """objectがletterを含んでいるかどうかを判定する"""
+        return True
+
+    res = result_list[0]
+    x1 = res.bounding_box.x
+    x2 = x1 + res.bounding_box.w
+
+    target_names = ["blue sign", "utility pole"]
+    res.rate += 1.0
 
     coordinate_utilitypole = []
     Coordinate_bulesign = []
